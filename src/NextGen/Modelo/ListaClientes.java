5e9b3d6e56c7b6eab12c1aeef380fa5884b3f0cb
gegexina <115779572+gegexina@users.noreply.github.com>
2023-10-28 19:06:55 +0200

    ok

---
+++ src/NextGen/Modelo/ListaClientes.java
@@ -0,0 +1,25 @@
+package NextGen.Modelo;
+/**
+ * Clase que representa una lista de clientes. Esta clase hereda de la clase genérica Lista<Cliente>.
+ */
+public class ListaClientes extends Lista<Cliente> {
+    /**
+     * Constructor para la lista de clientes. Inicializa una lista de clientes vacía.
+     */
+    public ListaClientes() {
+        super();
+    }
+    /**
+     * Busca un cliente en la lista por su NIF.
+     * @param nif El NIF del cliente que se desea buscar.
+     * @return El cliente con el NIF especificado, o null si no se encuentra.
+     */
+    public Cliente buscarPorNif(int nif) {
+        for (Cliente cliente : lista) {
+            if (cliente.getNif() == nif) {
+                return cliente;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/NextGen/Vista/GestionOS.java
@@ -0,0 +1,63 @@
+package NextGen.Vista;
+
+import NextGen.Controlador.Controlador;
+import java.util.Scanner;
+
+public class GestionOS {
+    private Controlador controlador;
+    Scanner teclado = new Scanner(System.in);
+
+    public GestionOS() {
+        controlador = new Controlador();
+    }
+
+    public void inicio() {
+        boolean salir = false;
+        char opcio;
+        do {
+            System.out.println("1. Gestión Articulos");
+            System.out.println("2. Gestión Clientes");
+            System.out.println("3. Gestión Pedidos");
+            System.out.println("0. Salir");
+            opcio = pedirOpcion();
+            switch (opcio) {
+                case '1':
+                    gestionArticulos();
+                    break;
+                case '2':
+                    gestionClientes();
+                    break;
+                case '3':
+                    gestionPedidos();
+                    break;
+                case '0':
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("Opción inválida. Por favor, elija una opción válida.");
+            }
+        } while (!salir);
+    }
+
+    private char pedirOpcion() {
+        String resp;
+        System.out.println("Elige una opción (1,2,3 o 0): ");
+        resp = teclado.nextLine();
+        if (resp.isEmpty()) {
+            resp = " ";
+        }
+        return resp.charAt(0);
+    }
+
+    public void gestionArticulos() {
+        //FALTA IMPLEMENTAR LA FUCNIÓN
+    }
+
+    public void gestionClientes() {
+        //FALTA IMPLEMENTAR LA FUCNIÓN
+    }
+
+    public void gestionPedidos() {
+        //FALTA IMPLEMENTAR LA FUCNIÓN
+    }
+}
